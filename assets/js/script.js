c914a63b394782e07935d2c0fb65a91ae168919a
Iryna <iryna.shulim@gmail.com>
2021-06-04 17:40:06 -0500

    local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,58 @@
 var currentDay = moment().format('LLL');
 $("#currentDay").append(currentDay);
 
-// time for task table
-var nineAm = moment("2021-06-03 9:00").format("h a");
-$("#nine-am").append(nineAm);
+var list = JSON.parse(localStorage.getItem('todolist')) || [];
 
-var tenAm = moment("2021-06-03 10:00").format("h a");
-$("#ten-am").append(tenAm);
+// Renders our to-dos to the page
+function renderTodos(list) {
+    // Empties out the html
+    $('#to-dos').empty();
 
-// create task input
-var tasks = {}; 
+// Iterates over the 'list'
+for (var i = 0; i < list.length; i++) {
+    // Creates a new variable 'toDoItem' that will hold a "<p>" tag
+    // Sets the `list` item's value as text of this <p> element
+    var toDoItem = $('<p>');
+    toDoItem.text(list[i]);
 
-var createTask = function(taskText) {
-    var taskP = $("<p>")
-    .addClass("p-text")
-    .text(taskText);
+    // Creates a button `toDoClose` with an attribute called `data-to-do` and a unique `id`
+    var toDoSave = $('<button>');
+    toDoSave.attr('data-to-do', i);
 
-$("#task-9am").append(taskP);
+// Adds the button to the `toDoItem`
+toDoItem = toDoItem.prepend(toDoSave);
+
+// Adds 'toDoItem' to the To-Do List div
+$('#to-dos').append(toDoItem);
 }
-// save tasks
-var saveTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  };
+}
+$('#checkbox').on('click', function(event) {
+    event.preventDefault();
+
+// Get the to-do "value" from the textbox and store it as a variable using `.val()` and `.trim()`
+        // YOUR CODE HERE
+        var toDo = $(".input-box")
+        .val()
+        .trim();
+        console.log(toDo);
+
+        // Add the new to-do to our local 'list' variable
+        // YOUR CODE HERE
+        list.push(toDo);
+        console.log(list)
+
+        // Update the to-dos on the page
+        // YOUR CODE HERE
+        for (i = 0; i<list.length; i++) {
+            var item = list[i];
+          };
+          renderTodos(list);
+
+          // Save the to-dos into localStorage
+        // We need to use JSON.stringify to turn the list from an array into a string
+        // YOUR CODE HERE
+        var task = JSON.stringify(list);
+        localStorage.setItem("todolist", task);
+    });
+
 
