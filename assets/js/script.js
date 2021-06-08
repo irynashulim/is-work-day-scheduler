53e5c13fd9cf7d7cbeecb9f659b059cafeabef3f
Iryna <iryna.shulim@gmail.com>
2021-06-08 18:52:08 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,58 @@
-//todays date on the top of the page
+$(document).ready(function () {
+
+  //todays date on the top of the page
 var currentDay = moment().format('LLL');
 $("#currentDay").append(currentDay);
+var time = moment().format("HH");
 
+//events for the schedule
+var events = ["", "", "", "", "", "", "", "", ""];
+var schedule = $(".container");
 
-
-var schedule = JSON.parse(localStorage.getItem('schedule')) || [];
-
-
-function renderTodos(schedule) {
-    $('#to-dos').empty();
-
-// Iterates over the 'list'
-for (var i = 0; i < schedule.length; i++) {
-    
-    var scheduleItem = $('<p>');
-    scheduleItem.text(schedule[i]);
-
-    var scheduleSave = $('<button>');
-    scheduleSave.attr('schedule-to-do', i);
-    scheduleSave.addClass('save');
-
-scheduleItem = scheduleItem.prepend(scheduleSave);
-
-$('#to-dos').append(scheduleItem);
+//get events from local storage
+if (localStorage.getItem("events")) {
+  events = JSON.parse(localStorage.getItem("events"));
 }
+
+// changing colors for past, current and future time
+for (var i = 0; i < 9; i++) {
+  var color 
+  if (time == i + 8) {
+    color = "present";
+  } else if (time < i + 8) {
+    color = "future";
+  } else {
+    color = "past";
+  }
+// creating row and colons for the schedule
+  var scheduleRow = $("<div class='row hour'></div>");
+  var timeSection = $("<div class='col-2 time-block'></div>");
+  var scheduleSection = $("<div class='col-8'></div>");
+  var btnSection = $("<div class='col-2'></div>");
+
+  timeSection.text(convertTime(i + 8));
+
+  var textArea = $("<textarea class='form-control textarea'></textarea>");
+  textArea.attr("id", i + "_textarea");
+  textArea.val(events[i]);
+  
+
+
+
 }
-$('#checkbox').on('click', function(event) {
-    event.preventDefault();
 
-        var toDo = $(".input-box")
-        .val()
-        .trim();
 
-        schedule.push(toDo);
 
-        for ( i = 0; i<schedule.length; i++) {
-            var item = schedule[i];
-          };
-          renderTodos(schedule);
 
-        var task = JSON.stringify(schedule);
-        localStorage.setItem("schedule", task);
-      });
 
-      $(document).on('click', '.save', function() {
-        var itemId = $(this).attr('schedule-to-do')
-        schedule.splice(itemId, 1);
-        renderTodos(schedule);
-        localStorage.setItem("schedule", JSON.stringify(schedule));
-      });
 
-      renderTodos(schedule);
-    
-      
+
+
+
+
+
+
+
+
+})
+
