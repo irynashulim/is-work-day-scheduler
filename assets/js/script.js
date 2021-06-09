c8cc674283e70a17d096a31a3268b3434b135a70
Iryna <iryna.shulim@gmail.com>
2021-06-08 21:38:30 -0500

    working code

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,14 +29,11 @@ for (var i = 0; i < 9; i++) {
   var timeSection = $("<div class='col-2 time-block'></div>");
   var scheduleSection = $("<div class='col-8'></div>");
   var btnSection = $("<div class='col-2'></div>");
-  scheduleRow.append(timeSection);
-  scheduleRow.append(scheduleSection);
-  scheduleRow.append(btnSection);
-  schedule.append(scheduleRow);
+  
 //timeSection
   timeSection.text(convertTime(i + 8));
 //scheduleSection
-  var textArea = $("<textarea class='form-control textarea'></textarea>");
+  var textArea = $("<textarea class='form-control textarea' aria-label='With textarea'></textarea>");
   textArea.attr("id", i + "_textarea");
   textArea.val(events[i]);
   textArea.addClass(color);
@@ -45,33 +42,34 @@ for (var i = 0; i < 9; i++) {
 var saveBtn = $("<button type='button' class='saveBtn'></button'>");
 saveBtn.text("Save");
 saveBtn.attr("id", i);
-saveBtn.onclick(function(event) {
+saveBtn.click(function(event) {
   save(event);
 });
 btnSection.append(saveBtn);
+scheduleRow.append(timeSection);
+  scheduleRow.append(scheduleSection);
+  scheduleRow.append(btnSection);
+  schedule.append(scheduleRow);
 
-
-
-
-
-
-
-
-
+//functions 
+//save() function
+function save(event) {
+  var id = event.target.id;
+  var text = $("#" + id + "_textarea").val();
+  events[id] = text;
+  console.log(events);
+  // saving to local storage
+  localStorage.setItem("events", JSON.stringify(events));
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+function convertTime(hour) {
+    if (hour > 12) {
+      var milHour = hour - 12;
+      return milHour + "pm";
+    } else {
+      return hour + "am";
+    }
+  }
+}
+});
 
